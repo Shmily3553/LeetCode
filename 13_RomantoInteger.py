a94d4307babb7b545a4dcc8eb6e5190dcd416e0d
Shmily3553 <caoyaree@hotmail.com>
2025-09-17 11:44:56 -0400

    update 13

---
+++ 13_RomantoInteger.py
@@ -0,0 +1,37 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        for i in range(len(s)):
+            if s[i] == 'M':
+                res += 1000
+            elif s[i] == 'D':
+                res += 500
+            elif s[i] == 'C':
+                if i+1 < len(s):
+                    if s[i+1] == 'D' or s[i+1] == 'M':
+                        res -= 100                        
+                    else:
+                        res += 100
+                else:
+                    res += 100
+            elif s[i] == 'L':
+                res += 50
+            elif s[i] == 'X':
+                if i+1 < len(s):
+                    if s[i+1] == 'L' or s[i+1] == 'C':
+                        res -= 10
+                    else:
+                        res += 10
+                else:
+                    res += 10
+            elif s[i] == 'V':
+                res += 5
+            elif s[i] == 'I':
+                if i+1 < len(s):
+                    if s[i+1] == 'V' or s[i+1] == 'X':
+                        res -= 1
+                    else:
+                        res += 1
+                else:
+                    res += 1
+        return res
